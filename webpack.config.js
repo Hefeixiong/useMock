d166ce059b863f742ff2cf17118fa2a9a8369ccf
hefeixiong <1074535830@qq.com>
2021-12-30 22:06:26 +0800

    useMock

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
-    optput: {
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
         filename: 'index.[contenthash].js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'mock',
+            template: './src/assets/index.html'
+        })
+    ]
 }
