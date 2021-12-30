d5805589e4858dfb92806328778e4081e7e2e7b7
hefeixiong <1074535830@qq.com>
2021-12-30 17:08:37 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Mock from 'mockjs';
+
+let data = Mock.mock({
+    'list|1-10' : [{
+        'id|+1' :1
+    }]
+})
+
+console.log(JSON.stringify(data, null , 4))
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    optput: {
+        filename: 'index.[contenthash].js'
+    }
+}
